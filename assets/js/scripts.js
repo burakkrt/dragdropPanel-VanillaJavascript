b70e6fee42ae4dc1107d2e3c2ab5dbd34b82d99f
BurakKurt <krtburak@outlook.com>
2023-04-17 02:11:32 +0300

    fix(new row): Fixed event listener conflict issue on newly added line.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,20 @@
 // Import components
 import getFormHtmlElement from './components/formElements.js';
-import addRowElement from './components/addRowElement.js';
 // DOM element
 const draggables = document.querySelectorAll('.draggable');
+const elements = document.querySelectorAll('.row-element');
 
 // Local veriables
 let dragtype;
 
-document.getElementById('addRow').addEventListener('click', addRowElement);
-
-function jquerySortable(rowid) {
+export function jquerySortable(rowid) {
   $(`#${rowid} .row-content`).sortable({
     placeholder: 'ui-state-highlight',
   });
   $(`#${rowid} .row-content`).disableSelection();
 }
 
-function addRowContent(rowid, formElementType) {
+export function addRowContent(rowid, formElementType) {
   const dropzone = document.querySelector(`#${rowid} .dropzone`);
   if (dropzone.classList.contains('null')) {
     dropzone.insertAdjacentHTML(
@@ -43,31 +41,61 @@ draggables.forEach(draggable => {
   draggable.addEventListener('dragend', () => {});
 });
 
-export function elementsAddeventListener() {
-  const elements = document.querySelectorAll('.row-element');
-  elements.forEach(element => {
-    element.addEventListener('dragover', e => {
-      if (e.target.classList.contains('dropzone')) {
-        e.preventDefault();
-      }
-    });
-
-    // element.addEventListener('dragleave', e => {
-    //   if (e.target.classList.contains('dropzone')) {
-    //   }
-    // });
-
-    element.addEventListener('drop', e => {
-      // console.log(dragtype);
-      // console.log(e.target);
-      const rowid = e.target.parentNode.getAttribute('id');
-      addRowContent(rowid, dragtype);
-      jquerySortable(rowid);
-    });
+elements.forEach(element => {
+  element.addEventListener('dragover', e => {
+    if (e.target.classList.contains('dropzone')) {
+      e.preventDefault();
+    }
   });
-}
 
-elementsAddeventListener();
+  element.addEventListener('drop', e => {
+    const rowid = e.target.parentNode.getAttribute('id');
+    addRowContent(rowid, dragtype);
+    jquerySortable(rowid);
+  });
+});
+
+document.getElementById('addRow').addEventListener('click', () => {
+  const rowCount = document.querySelector('.main-content').children.length;
+  const newId = `row${rowCount}`;
+  const rowElement = `
+    <div class="row-element d-flex flex-column gap-3 p-3 rounded-3 mb-3" id="${newId}">
+      <div class="row-element-header">
+        <div class="d-flex flex-row justify-content-between align-items-center">
+          <h5>Row ${rowCount}</h5>
+          <h5>Buttons</h5>
+        </div>
+      </div>
+      <div class="dropzone null flex-grow-1 w-75 p-4 rounded-4 mx-auto">
+        <div
+          class="drag-info d-flex flex-column align-items-center justify-content-center row-gap-3 column-gap-1 pe-none"
+        >
+          <span
+            class="dropicon material-symbols-outlined"
+            style="font-size: 32px; animation: dropzoneIconAnimated 2s infinite ease-in-out"
+          >
+            pin_drop
+          </span>
+          <span>Drag and drop the element you want to add.</span>
+        </div>
+      </div>
+    </div>
+  `;
+  document
+    .querySelector('.main-content')
+    .lastElementChild.insertAdjacentHTML('beforebegin', rowElement);
+
+  document.querySelector(`#${newId}`).addEventListener('dragover', e => {
+    if (e.target.classList.contains('dropzone')) {
+      e.preventDefault();
+    }
+  });
+
+  document.querySelector(`#${newId}`).addEventListener('drop', () => {
+    addRowContent(`${newId}`, dragtype);
+    jquerySortable(`${newId}`);
+  });
+});
 
 // OPTIMIZE:Bazen form elementlerinin sortable ile yerini
 //  değiştirdiğinde takılamlar oluşuyor ona çözüm bul.
